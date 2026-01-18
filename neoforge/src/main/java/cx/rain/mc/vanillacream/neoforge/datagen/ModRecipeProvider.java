bf4db54bd8fee5b2609e8809ad4ecc18f0bc937d
秋雨落 <i@rain.cx>
2026-01-18 20:38:00 +0800

    Fix recipe namespace.
    Signed-off-by: 秋雨落 <i@rain.cx>

--- neoforge/src/main/java/cx/rain/mc/vanillacream/neoforge/datagen/ModRecipeProvider.java
+++ neoforge/src/main/java/cx/rain/mc/vanillacream/neoforge/datagen/ModRecipeProvider.java
@@ -8,6 +8,8 @@ import net.minecraft.world.item.Items;
 
 import java.util.concurrent.CompletableFuture;
 
+import static cx.rain.mc.vanillacream.util.ResourceLocationHelper.modLoc;
+
 public class ModRecipeProvider extends RecipeProvider {
     public ModRecipeProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries);
@@ -28,27 +30,27 @@ public class ModRecipeProvider extends RecipeProvider {
                 .requires(Items.DRIPSTONE_BLOCK)
                 .unlockedBy("has_diorite", has(Items.DIORITE))
                 .unlockedBy("has_dripstone_block", has(Items.DRIPSTONE_BLOCK))
-                .save(recipeOutput);
+                .save(recipeOutput, modLoc("calcite"));
 
         ShapelessRecipeBuilder.shapeless(RecipeCategory.BUILDING_BLOCKS, Items.TUFF, 2)
                 .requires(Items.ANDESITE)
                 .requires(Items.BASALT)
                 .unlockedBy("has_andesite", has(Items.ANDESITE))
                 .unlockedBy("has_basalt", has(Items.BASALT))
-                .save(recipeOutput);
+                .save(recipeOutput, modLoc("tuff"));
 
         ShapelessRecipeBuilder.shapeless(RecipeCategory.BUILDING_BLOCKS, Items.DEEPSLATE, 2)
                 .requires(Items.STONE)
                 .requires(Items.BASALT)
                 .unlockedBy("has_stone", has(Items.STONE))
                 .unlockedBy("has_basalt", has(Items.BASALT))
-                .save(recipeOutput);
+                .save(recipeOutput, modLoc("deepslate"));
 
         ShapelessRecipeBuilder.shapeless(RecipeCategory.BUILDING_BLOCKS, Items.BLACKSTONE, 2)
                 .requires(Items.DEEPSLATE)
                 .requires(Items.BASALT)
                 .unlockedBy("has_stone", has(Items.STONE))
                 .unlockedBy("has_basalt", has(Items.BASALT))
-                .save(recipeOutput);
+                .save(recipeOutput, modLoc("blackstone"));
     }
 }
